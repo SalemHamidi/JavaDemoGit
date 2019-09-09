4f391385a60db7de108b58a788daf6faad2e3057
SalemHamidi <43269413+SalemHamidi@users.noreply.github.com>
2019-09-09 20:57:22 +0200

    First git commit with java project

--- src/com/demogit/Main.java
+++ src/com/demogit/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("This is my first git project with intellij");
+        System.out.println("This s my first git project with intellij");
     }
 }
